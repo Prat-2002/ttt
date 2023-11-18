c92fda7a793cce0c40da38440e9d1b1c616d0fab
Pratham Panwar <94755712+Prat-2002@users.noreply.github.com>
2023-11-19 02:32:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileContent from "./components/body"
+import ProfileContent2 from "./components/body2"
+import ProfileContent3 from "./components/body3"
+import Header from "./components/header"
+import Profile from "./components/profile"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <>
+    <Header />
+    <Profile/>
+    <ProfileContent/>
+    <hr/>
+    <ProfileContent2 />
+    <hr />
+    <ProfileContent3/>
+    </>
+  )
 }
-
-export default App;
